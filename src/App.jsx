db6eb3c9d03677a8b58f1a25f7909742720287e8
spenceruns <spenceruns@gmail.com>
2020-09-09 10:20:20 -0700

    Created auto complete address search using Google Maps Places API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css'
+import AutocompleteInput from './components/search-bar';
 
 function App() {
+  let [address, setAddress] = useState('')
+
   return (
-    <div className="App">
-      Hello
+    <div className="app">
+      <input type="text" placeholder="Search"/>
+      <AutocompleteInput />
+      <input type="number" min="1" max="50"/>
+      <button>Click Me!</button>
     </div>
   );
 }
