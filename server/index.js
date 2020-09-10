3559acad9f2080786152c1aaad182036495501b8
spenceruns <spenceruns@gmail.com>
2020-09-10 11:52:27 -0700

    changed server port

--- server/index.js
+++ server/index.js
@@ -25,6 +25,6 @@ app.get('/api/yelpsearch', (req, res, next) => {
     .catch(err => console.error(err));
 });
 
-app.listen(3001, () => {
-  console.log('Listening on port 3001');
+app.listen(3002, () => {
+  console.log('Listening on port 3002');
 });
